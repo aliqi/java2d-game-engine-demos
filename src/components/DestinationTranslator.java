5c6886c1c9077ce6bf0a7dcf743e4bf3a1d9bc45
ZhaoYan <aliqi@hotmail.com>
2023-04-19 08:12:29 +0800

    Add DestinationTranslator.

---
+++ src/components/DestinationTranslator.java
@@ -0,0 +1,59 @@
+package components;
+
+import java2d.game.GameComponent;
+import java2d.game.Maths;
+import java2d.game.Time;
+import java2d.game.Transform;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Author:     Zhao Yan
+ * DateTime:   2023/4/19 7:49
+ */
+public class DestinationTranslator extends GameComponent {
+
+    public final Point2D destination = new Point2D.Double();
+
+    public double speed = 1d;
+
+    public ReachedEvent reachedEvent;
+
+    private boolean moving;
+
+    public void begin() {
+        moving = true;
+    }
+
+    public void end() {
+        moving = false;
+    }
+
+    @Override
+    protected void update() {
+        if (moving) {
+            Transform transform = getTransform();
+            Point2D position = transform.getPosition();
+
+            if (Maths.distance(position, destination) <= 0.001) {
+                transform.setPosition(destination);
+
+                if (reachedEvent != null)
+                    reachedEvent.reached(this);
+
+                moving = false;
+            } else {
+                position = Maths.moveTowards(position, destination,
+                        speed * Time.deltaTime * 100);
+
+                transform.setPosition(position);
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface ReachedEvent {
+
+        void reached(DestinationTranslator translator);
+    }
+}
--- src/demo8/Demo.java
+++ src/demo8/Demo.java
@@ -1,8 +1,11 @@
 package demo8;
 
+import components.DestinationTranslator;
 import java2d.game.Game;
 import java2d.game.GameScene;
+import java2d.game.SpriteGameObject;
 import java2d.game.ui.Button;
+import models.Ship;
 
 public class Demo {
 
@@ -10,12 +13,19 @@ public class Demo {
         Game game = new Game();
         GameScene scene = game.getScene();
 
+        SpriteGameObject ship = new SpriteGameObject("assets/sprites/spacecraft1.png");
+        DestinationTranslator translator = new DestinationTranslator();
+        ship.addComponent(translator);
+        scene.add(ship);
+
         Button btnTest = new Button();
         btnTest.text.content = "Test";
         btnTest.setSize(100, 50);
         btnTest.transform.setPosition(100, 100);
         btnTest.clickedEvent = (btn, mouseBtn) -> {
-            System.out.println("Test~");
+            translator.destination.setLocation(100, 100);
+            translator.reachedEvent = t -> System.out.println("Reached");
+            translator.begin();
         };
 
         scene.ui.add(btnTest);
